7281cfba725842b1139a3441f92f97697b7802fd
atalantus <contact@atalantus.de>
2018-05-17 16:24:45 +0200

    Added TodoItem Backend

---
+++ SurvivalBox_App/SurvivalBox/SurvivalBox/SurvivalBox/Services/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace SurvivalBox.Services
+{
+    public static class Constants
+    {
+        // Replace string with your mobile service URL.
+        public static string ApplicationURL = @"https://survivalbox.azurewebsites.net";
+    }
+}
---
+++ SurvivalBox_App/SurvivalBox/SurvivalBox/SurvivalBox/Services/ServerConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using SurvivalBox.Models;
+
+namespace SurvivalBox.Services
+{
+    public class ServerConnection
+    {
+        private static ServerConnection _defaultInstance = null;
+        public static ServerConnection DefaultInstance => _defaultInstance ?? (_defaultInstance = new ServerConnection());
+
+        private readonly MobileServiceClient _client;
+        public MobileServiceClient Client => _client;
+
+        private readonly MobileServiceSQLiteStore _store;
+        public MobileServiceSQLiteStore Store => _store;
+
+        public ServerConnection()
+        {
+            _client = new MobileServiceClient(Constants.ApplicationURL);
+            _store = new MobileServiceSQLiteStore("localstore.db");
+            _client.SyncContext.InitializeAsync(_store);
+        }
+    }
+}
